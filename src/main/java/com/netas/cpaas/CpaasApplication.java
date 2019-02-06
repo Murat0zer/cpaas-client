ab9d576f97421d29a45c54e9ad28ed7d112adedf
Murat OZER <mozer@netas.lab.nortel.com>
2019-02-06 14:20:59 +0300

    initial commit

---
+++ src/main/java/com/netas/cpaas/CpaasApplication.java
@@ -0,0 +1,14 @@
+package com.netas.cpaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpaasApplication.class, args);
+	}
+
+}
+
